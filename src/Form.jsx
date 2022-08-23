be46cd19b4b6df8b61f503023c5359c30415f6ae
mikiyaaa <katocha.m0220thankyou@gmail.com>
2022-08-23 23:56:16 +0900

    Formコンポーネント作成

---
+++ src/Form.jsx
@@ -0,0 +1,10 @@
+export const Form = () => {
+
+    return (
+        <form action="">
+            <label htmlFor=""><b>フォーム</b></label><br />
+            <input type="text" /><br />
+            <input type="button" value="送信" />
+        </form>
+    )
+}
