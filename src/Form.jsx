93fb4ddf6b77a9b52b7083186ab603cb425bddb2
mikiyaaa <katocha.m0220thankyou@gmail.com>
2022-08-24 20:13:14 +0900

    子コンポーネントから親コンポーネントを関数で呼び出し

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 
 export class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+    }
+
+    submitForm(e) {
+        e.preventDefault();
+        this.props.onAddFrameWork(this.state.text); // 親コンポーネントの関数を呼び出す
+    }
 
     render() {
+        const { text } = this.state;
         return (
-            <form action="">
-                <label htmlFor="">フォーム</label><br/>
-                <input type="text" /><br/>
-                <input type="button" value="送信" />
-            </form>
+            <div>
+                <h5>新しいライブラリを追加</h5>
+                <form onSubmit={(e) => {this.submitForm(e)}}>
+                    {/* 入力フォームに変更があるたびにsteteを変更する */}
+                    <input type="text" value={text} onChange={(e) => {this.setState({ text: e.target.value })}} /><br/>
+                    <button>送信</button>
+                </form>
+            </div>
         )
     }
 }
