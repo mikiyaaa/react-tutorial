0bbb955990ce76fc94e753e6cc208b091ef93754
mikiyaaa <katocha.m0220thankyou@gmail.com>
2022-08-24 23:27:44 +0900

    2秒後に配列データを返す関数

--- src/const/js_frameworks.js
+++ src/const/js_frameworks.js
@@ -1,5 +1,13 @@
-export const JS_FRAMEWORKS = [
+const JS_FRAMEWORKS = [
     'React',
     'Vue',
     'Angular',
 ];
+
+export const getFrameworks = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(JS_FRAMEWORKS);
+        }, 2000)
+    })
+}
